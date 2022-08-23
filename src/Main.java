c69ededd1992814c62e9b7b0643117d5aff2e8a2
PopovaElis <elizprest@gmail.com>
2022-08-23 21:42:13 +0500

    1 задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+public class  Main {
+
+     public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+
+        return arr;
+     }
+     //Задание 1
+        public static void main (String[] args) {
+
+
+            int[] arr = generateRandomArray();
+            int sum = 0;
+            for (int i = 0; i < arr.length; i++){
+                sum+= arr[i];
+         }
+                System.out.println("Сумма трат за месяц составила "+ sum + " рублей");
+            }
+        }
+
