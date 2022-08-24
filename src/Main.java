2d70a006a65f7b0e174785386f95b5ea13f2c725
PopovaElis <elizprest@gmail.com>
2022-08-24 21:33:47 +0500

    3 задание

--- src/Main.java
+++ src/Main.java
@@ -22,14 +22,32 @@ public class  Main {
                 System.out.println("Сумма трат за месяц составила "+ sum + " рублей");
 
     //Задание 2
-    int max=1;
+    int max=arr[0];
     for (int i=0; i< arr.length;i++){
         if (arr[i] > max) {
             max=arr[i];
         }
     }
     System.out.println("Максимальная сумма трат за день составила "+max+"рублей");
-        }
+            //Задание 2.1
+            int min= arr[0]; ;
+            for (int i=0; i< arr.length;i++){
+                   if (arr[i] < min) {
+min = arr[i];
+                    }
+                }
+            System.out.println("Минимальная сумма трат за день составила "+min +"рублей");
+
+    //Задание 3
+     int mean = sum/arr.length;
+            System.out.println("Средняя сумма трат за месяц составила "+mean+ "рублей");
+   //Задание 4
+            char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+
 }
+}
+
+
 
 
