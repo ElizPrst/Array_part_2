cc298665fd9e133588ac07f81553818cefdf56f7
PopovaElis <elizprest@gmail.com>
2022-08-24 10:55:56 +0500

    2 задание

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,18 @@ public class  Main {
             for (int i = 0; i < arr.length; i++){
                 sum+= arr[i];
          }
+
                 System.out.println("Сумма трат за месяц составила "+ sum + " рублей");
-            }
+
+    //Задание 2
+    int max=1;
+    for (int i=0; i< arr.length;i++){
+        if (arr[i] > max) {
+            max=arr[i];
         }
+    }
+    System.out.println("Максимальная сумма трат за день составила "+max+"рублей");
+        }
+}
+
 
